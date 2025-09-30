0e09625508f543aaa67bf165a00c37260ea91973
andieazril <173057907+andieazril@users.noreply.github.com.>
2025-09-30 16:37:13 +0700

    add

---
+++ P/Student.java
@@ -0,0 +1,28 @@
+class Student {
+    private String nim;
+    private String name;
+    private String major;
+    
+    public Student(String nim, String name, String major) {
+        this.nim = nim;
+        this.name = name;
+        this.major = major;
+    }
+    
+    public String getNim() {
+        return nim;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getMajor() {
+        return major;
+    }
+    
+    @Override
+    public String toString() {
+        return "NIM: " + nim + " | Nama: " + name + " | Jurusan: " + major;
+    }
+}
